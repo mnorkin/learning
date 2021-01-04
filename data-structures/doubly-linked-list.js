024fb5d7145186430578a9d8fc4e552b18638e0e
mnorkin <maksimn@wix.com>
2021-01-04 21:39:56 +0200

    Adding nm

--- data-structures/doubly-linked-list.js
+++ data-structures/doubly-linked-list.js
@@ -26,6 +26,35 @@ class DoublyLinkedList {
     }
     this.length++;
   }
+
+  remove(data) {
+    let current = this.head;
+    while (current) {
+      if (current.data === data) {
+        // if the element is the head
+        if (current === this.head) {
+          // set the head as the next, after head
+          this.head = this.head.next;
+        }
+        // if the element is the tail
+        else if (current === this.tail) {
+          // set the tail as the previous, before the tail
+          this.tail = this.tail.previous;
+          this.tail.next = null;
+        }
+        // in the middle
+        else {
+          // set current's previous next as current's next element
+          current.previous.next = current.next;
+          current.next.previous = current.previous;
+        }
+        this.length--;
+        break;
+      }
+
+      current = current.next;
+    }
+  }
 }
 
 
